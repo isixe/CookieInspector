789ef232393ee137ce94cd6ca4db245bb1620589
isixe <isixe@outlook.com>
2025-04-01 21:50:58 +0800

    🎈 perf(core): update cookie core parsing function

--- core/parsed.ts
+++ core/parsed.ts
@@ -1,54 +1,54 @@
 /**
  * Cookie parsed core
- * @name cookieString The cookie string to parse
- * @name ParsedCookie[] An array of parsed cookies list
  */
 
-import { CookieType, ParsedCookie } from '@/types/cookie'
+import { ParsedCookie, SubParsedCookie } from '@/types/cookie'
 import { getStringType } from '@/utils/stringType'
 
 /**
- * Parse a cookie string from the parsed cookies list
+ * ------------------------------------------------------------------------
+ * ParsedCookie | cookieString
+ * @name cookieString The cookie string to parse
+ * @name ParsedCookie[] An array of parsed cookie list
+ * ------------------------------------------------------------------------
+ */
+
+/**
+ * Parse a ParsedCookie list to cookie string
+ * @param parsedCookies The ParsedCookie list to parse
  * @returns string: The origin cookie string
  */
-export function parseToOriginString() {
-  return
+export function parseToOriginString(parsedCookie: ParsedCookie[]): string {
+  if (!parsedCookie || parsedCookie.length === 0) {
+    return ''
+  }
+
+  const cookieString = parsedCookie
+    .map((row) => `${row.name}=${row.value}`)
+    .join(';')
+
+  return cookieString
 }
 
 /**
- * Parse a cookie string from the origin cookie string format
- * @param newCookieString The new cookie string to parse
- * @returns ParsedCookie[]: An array of parsed cookies list
+ * Parse the origin cookie string to a ParsedCookie list
+ * @param cookieString The new cookie string to parse
+ * @returns ParsedCookie[]: An array of parsed cookie list
  */
-export function parseFromOriginString(newCookieString: string): ParsedCookie[] {
-  if (!newCookieString) {
+export function parseFromOriginString(cookieString: string): ParsedCookie[] {
+  if (!cookieString || !cookieString.trim()) {
     return []
   }
 
-  const cookieList = newCookieString.split(';')
+  const cookieList = cookieString.split(';')
 
   let parsedCookies: ParsedCookie[] = []
 
   cookieList.forEach((rowCookie, index) => {
-    let subValues: { name: string; value: string; type: CookieType }[] = []
-
-    if (rowCookie.indexOf('&') > 0) {
-      rowCookie.split('&').forEach((subCookie) => {
-        const [name, value] = subCookie.split('=')
-        if (name) {
-          subValues.push({
-            name,
-            value,
-            type: getStringType(value)
-          })
-        }
-      })
-    }
-
     const rowName = rowCookie.split('=')[0]
     const rowValue = rowCookie.substring(rowCookie.indexOf('=') + 1)
     const type = getStringType(rowValue)
-    subValues = subValues.filter((subValue) => subValue !== undefined)
+    const subValues = subParseFromRowCookieString(rowCookie)
 
     const rowParsedCookie = {
       id: `${index}`,
@@ -62,4 +62,144 @@ export function parseFromOriginString(newCookieString: string): ParsedCookie[] {
   })
 
   return parsedCookies
+}
+
+/**
+ * ------------------------------------------------------------------------
+ * rowCookieString | SubParsedCookie
+ * @name rowCookieString The row cookie string to parse
+ * @name SubParsedCookie An array of sub parsed cookies
+ * ------------------------------------------------------------------------
+ */
+
+/**
+ * Parse a raw cookie string to a SubParsedCookie list
+ * @param rowCookieString The row cookie string to parse
+ * @returns SubParsedCookie[] An array of sub parsed cookies
+ */
+export function subParseFromRowCookieString(
+  rowCookieString: string
+): SubParsedCookie[] {
+  if (!rowCookieString) {
+    return []
+  }
+
+  const subValues: SubParsedCookie[] = []
+
+  if (rowCookieString.indexOf('&') > 0) {
+    rowCookieString.split('&').forEach((subCookie) => {
+      const [name, value] = subCookie.split('=')
+      if (name) {
+        const subParsedCookie = {
+          name,
+          value,
+          type: getStringType(value)
+        }
+
+        subValues.push(subParsedCookie)
+      }
+    })
+  }
+
+  return subValues.filter((subValue) => subValue !== undefined)
+}
+
+/**
+ * Parse a SubParsedCookie list to row cookie string
+ * @param subValues The SubParsedCookie list to parse
+ * @returns string: The row cookie string
+ */
+export function subParseToRowCookieString(
+  subValues: SubParsedCookie[]
+): string {
+  const rowCookieString = subValues
+    .map((subValue) => {
+      if (subValue.value) {
+        return `${subValue.name}=${subValue.value}`
+      }
+      return `${subValue.name}=`
+    })
+    .join('&')
+
+  return rowCookieString
+}
+
+/**
+ * ------------------------------------------------------------------------
+ * rowValue [->] ParsedCookie[]
+ * @name rowValue a row string cookie
+ * @name ParsedCookie[] An array of parsed cookies list
+ * ------------------------------------------------------------------------
+ */
+
+/**
+ * Update the parsed cookies list with the new item
+ * @param originParsedCookie The original parsed cookies list
+ * @param rowId The id of the cookie to update
+ * @param rowValue The value of the cookie to update
+ * @param rowName The name of the cookie to update
+ * @return ParsedCookie[]: The updated parsed cookies list
+ */
+export function updateRowToOriginParsedCookie(
+  originParsedCookie: ParsedCookie[],
+  rowId: string,
+  rowValue: string,
+  rowName?: string
+): ParsedCookie[] {
+  const updatedCookies = originParsedCookie.map((cookie) => {
+    if (cookie.id == rowId) {
+      const name = rowName || cookie.name
+      const value = rowValue
+      const type = getStringType(rowValue)
+      const subValues = subParseFromRowCookieString(`${name}=${value}`)
+
+      return {
+        ...cookie,
+        name,
+        value,
+        type,
+        subValues
+      }
+    }
+    return cookie
+  })
+  return updatedCookies
+}
+
+/**
+ * ------------------------------------------------------------------------
+ * subRowValue [->] SubParsedCookie[]
+ * @name subRowValue a sub row string cookie
+ * @name SubParsedCookie[] An array of sub parsed cookies list
+ * ------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @param subRowName
+ * @param subRowValue
+ * @returns SubParsedCookie[] a SubParsedCookie list
+ */
+export function updateSubRowToOriginSubParsedCookie(
+  originSubParsedCookies: SubParsedCookie[],
+  subRowIndex: Number,
+  subRowValue: string
+): SubParsedCookie[] {
+  let newSubParsedCookies = originSubParsedCookies.map((subValue, index) => {
+    if (subRowIndex === index) {
+      return {
+        name: subValue.name,
+        value: subRowValue,
+        type: getStringType(subRowValue)
+      }
+    }
+    return subValue
+  })
+
+  if (subRowValue.indexOf('&') > -1) {
+    const rowCookieString = subParseToRowCookieString(newSubParsedCookies)
+    newSubParsedCookies = subParseFromRowCookieString(rowCookieString)
+  }
+
+  return newSubParsedCookies
 }
